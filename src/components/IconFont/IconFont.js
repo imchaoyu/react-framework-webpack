968ba394f4677df9fdbc21a6f623fa338f83d037
chaoyu <chaoyumail@126.com>
2022-09-06 16:49:18 +0800

    feat: 自定义icon样式

--- src/components/IconFont/IconFont.js
+++ src/components/IconFont/IconFont.js
@@ -1,11 +1,16 @@
 import { createFromIconfontCN } from '@ant-design/icons';
 const IconFont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/c/font_3396634_mr03w23gsof.js',
+  scriptUrl: '//at.alicdn.com/t/c/font_3396634_54pkdxup915.js',
 });
 
 const Icon = (props) => {
   const icon = `icon-${props.name}`;
-  return <IconFont type={icon} />;
+  return (
+    <IconFont
+      type={icon}
+      style={{ fontSize: props.fontSize ?? '16px', color: props.color ?? '#fff' }}
+    />
+  );
 };
 
 export default Icon;
