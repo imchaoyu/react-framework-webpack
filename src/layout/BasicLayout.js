d957ba3734059510c3c1f66438e1015a116b1e6e
chaoyu <chaoyumail@126.com>
2022-09-07 16:40:39 +0800

    feat: 面包屑功能

--- src/layout/BasicLayout.js
+++ src/layout/BasicLayout.js
@@ -3,9 +3,10 @@ import { routes } from '@/routers/routes';
 import { Breadcrumb, Layout, Menu } from 'antd';
 import { useState } from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { Link, Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
 const { Header, Content, Footer, Sider } = Layout;
 
+// antd menu数据转化
 const getItem = (item) => {
   if (!item.name) return;
   const icon = item.icon && <Icon name={item.icon} />;
@@ -18,6 +19,7 @@ const getItem = (item) => {
     title,
   };
 };
+// 根据路由配置生成菜单
 const getMenuData = (data = routes, parentAuthority) => {
   const firstRoute = data[0];
   if (!firstRoute.layout && !firstRoute.name && !firstRoute.icon && firstRoute.children) {
@@ -34,21 +36,59 @@ const getMenuData = (data = routes, parentAuthority) => {
         const children = getMenuData(item.children, item.authority);
         result.children = children;
       }
+      console.count('getMenuData');
       return result;
     }
   });
+};
+
+// 根据路由配置生成面包屑
+const getBreadcrumbNameMap = () => {
+  const routerMap = {};
+  const mergeMenuAndRouter = (data) => {
+    data.forEach((menuItem) => {
+      if (menuItem.children) {
+        mergeMenuAndRouter(menuItem.children);
+      }
+      // Reduce memory usage
+      if (menuItem.path !== '/' && menuItem.name) {
+        routerMap[menuItem.path] = menuItem.name;
+      }
+    });
+  };
+  mergeMenuAndRouter(routes[0].children);
+  return routerMap;
 };
 
 const title = '测试-';
 console.time('menu');
 const menuData = getMenuData().filter(Boolean);
 console.timeEnd('menu');
+const breadcrumbMaps = getBreadcrumbNameMap();
+console.log('breadcrumbMaps: ', breadcrumbMaps);
 const BasicLayout = (props) => {
   const navigate = useNavigate();
   const location = useLocation();
   const params = useParams();
 
-  console.log('menuData: ', menuData);
+  const pathSnippets = location.pathname.split('/').filter((i) => i);
+  // 面包屑
+  const extraBreadcrumbItems = pathSnippets.map((_, index) => {
+    const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+    return (
+      <Breadcrumb.Item key={url}>
+        <Link to={url}>{breadcrumbMaps[url]}</Link>
+      </Breadcrumb.Item>
+    );
+  });
+
+  const breadcrumbItems = [
+    <Breadcrumb.Item key="home">
+      <Link to="/">首页</Link>
+    </Breadcrumb.Item>,
+  ].concat(extraBreadcrumbItems);
+
+  console.count('menuData: ');
   const [title, setTitle] = useState('');
 
   const [collapsed, setCollapsed] = useState(false);
@@ -83,12 +123,8 @@ const BasicLayout = (props) => {
           <Header
             className="site-layout-background"
             style={{
-              padding: 0,
-            }}
-          />
-          <Content
-            style={{
-              margin: '0 16px',
+              padding: '0 10px',
+              background: '#fff',
             }}
           >
             <Breadcrumb
@@ -96,9 +132,14 @@ const BasicLayout = (props) => {
                 margin: '16px 0',
               }}
             >
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              {breadcrumbItems}
             </Breadcrumb>
+          </Header>
+          <Content
+            style={{
+              margin: '16px',
+            }}
+          >
             <Outlet />
           </Content>
           <Footer
